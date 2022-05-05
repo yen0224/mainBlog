a37323bca03d39766503e270643fba3b7bdf48b0
yen0224 <yen0224@pm.me>
2022-05-05 19:07:00 +0800

    add: article router

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -7,20 +7,22 @@ router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+/*
 router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() })
 })
-
+*/
 router.get('/edit/:id', async (req, res) => {
   const article = await Article.findById(req.params.id)
-  res.render('articles/edit', { article: article })
+  //res.render('articles/edit', { article: article })
+  res.send(article)
 })
 
 router.get('/:slug', async (req, res) => {
   const article = await Article.findOne({ slug: req.params.slug })
   if (article == null) res.redirect('/')
-  res.render('articles/show', { article: article })
+  //res.render('articles/show', { article: article })
+  res.send(article)
 })
 
 router.post('/', async (req, res, next) => {
