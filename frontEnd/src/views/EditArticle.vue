c1742d43cadc70f8eea38e149e8ae47683d99c9a
yen0224 <yen0224@pm.me>
2022-05-23 20:50:46 +0800

    before checkout

--- frontEnd/src/views/EditArticle.vue
+++ frontEnd/src/views/EditArticle.vue
@@ -13,24 +13,5 @@ export default {
   components: {
     formField,
   },
-  data() {
-    return {
-      posts: [],
-    };
-  },
-  methods: {
-    async getData() {
-      try {
-        let response = await fetch("http://localhost:5555/articles" + post.id);
-        this.posts = await response.json();
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
-  created() {
-    this.getData();
-  },
 };
 </script>
